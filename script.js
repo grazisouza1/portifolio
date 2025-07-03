ad02b62b7325a0d7ff85174768bed4802fdd0e77
grazisouza1 <graziisouza0127@gmail.com>
2025-07-03 13:41:16 -0300

    feat: adding technologies section

--- script.js
+++ script.js
@@ -5,31 +5,7 @@ iconsNavBar.forEach((icon) => {
     iconsNavBar.forEach((i) => i.classList.remove("active"));
     this.classList.add("active");
   });
-});
-
-const filterBtns = document.querySelectorAll(".filter-btn");
-const certificateCards = document.querySelectorAll(".certificate-card");
-
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const filter = btn.innerHTML;
-
-    certificateCards.forEach((card) => {
-      if (filter === "All" || card.innerHTML.includes(filter)) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
-  });
-});
-
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    filterBtns.forEach((i) => i.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
+})
 
 //SCROLL EFFECT
 const observer = new IntersectionObserver((entries) => {
